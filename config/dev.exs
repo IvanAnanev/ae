23483f766c5d3d9c4dcbe38f432675dfee77e33d
Ivan Ananev <ivcheg@gmail.com>
2020-07-10 13:14:53 +0300

    install credo and test.watch

--- config/dev.exs
+++ config/dev.exs
@@ -74,3 +74,11 @@ config :phoenix, :plug_init_mode, :runtime
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+
+config :mix_test_watch,
+  tasks: [
+    "format --check-formatted",
+    "credo",
+    "test"
+  ],
+  clear: true
